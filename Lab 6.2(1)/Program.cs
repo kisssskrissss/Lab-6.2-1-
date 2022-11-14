bbcb03aae9c9a164f44e1b07ccdddec09d3e52a1
krist <krist@LAPTOP-HIQLHLL6>
2022-11-14 08:47:29 +0500

    Добавьте файлы проекта.

---
+++ Lab 6.2(1)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            str = str.Replace(" ", "");
+            str = str.ToLower();
+            string str1 = "";
+
+            foreach (char c in str)
+            {
+                str1 = c + str1;
+            }
+            Console.WriteLine(str1);
+            Console.WriteLine(str.Equals(str1));
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
